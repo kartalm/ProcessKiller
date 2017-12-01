66c0d2dba5ab0bcf23062b50282fae8a00b2755e
Mehmet KARTAL <mehmetbariskartal@gmail.com>
2017-12-01 14:54:32 +0300

    Add files via upload

---
+++ ProcessKillerConsoleApp/MrpProcess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessKillerConsoleApp
+{
+    public class MrpProcess
+    {
+        public System.Diagnostics.Process Process { get; set; }
+
+        public TimeSpan TotalProcessTime { get; set; }
+
+        public DateTime IdleSince { get; set; }
+
+        public bool Flag { get; set; }
+
+    }
+}
